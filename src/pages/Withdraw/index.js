3f420b78c99f56d7fa9e95d79499175577690ec1
Max Alekseenko <nnwen0609@gmail.com>
2022-03-25 18:04:20 +0800

    make withdrawal address required

--- src/pages/Withdraw/index.js
+++ src/pages/Withdraw/index.js
@@ -1,12 +1,12 @@
 import React, { useState, useCallback, useContext } from 'react';
-import { useWeb3React } from '@web3-react/core';
+// import { useWeb3React } from '@web3-react/core';
 
 import TransferInput from 'containers/TransferInput';
 import { ZkAccountContext } from 'contexts';
 
 import Card from 'components/Card';
 import Button from 'components/Button';
-import Checkbox from 'components/Checkbox';
+// import Checkbox from 'components/Checkbox';
 import Input from 'components/Input';
 
 const note = 'Amount withdrawn from zero knowledge pool will be deposited to the selected account.';
@@ -14,20 +14,23 @@ const note = 'Amount withdrawn from zero knowledge pool will be deposited to the
 export default () => {
   const { balance } = useContext(ZkAccountContext);
   const [amount, setAmount] = useState(0);
-  const { account } = useWeb3React();
+  const [receiver, setReceiver] = useState(null);
   const { withdraw } = useContext(ZkAccountContext);
-  const [isXDaiAddress, setIsXDaiAddress] = useState(false);
-  const handleCheckboxClick = useCallback(() => {
-    setIsXDaiAddress(!isXDaiAddress);
-  }, [isXDaiAddress]);
+  const handleReceiverChange = useCallback(e => {
+    setReceiver(e.target.value);
+  }, [setAmount]);
+  // const [isXDaiAddress, setIsXDaiAddress] = useState(false);
+  // const handleCheckboxClick = useCallback(() => {
+  //   setIsXDaiAddress(!isXDaiAddress);
+  // }, [isXDaiAddress]);
   return (
     <Card title="Withdraw" note={note}>
       <TransferInput balance={balance} amount={amount} setAmount={setAmount} isPoolToken={true} />
-      <Checkbox label="xDai address of receiver" check={isXDaiAddress} onChange={handleCheckboxClick} />
-      {isXDaiAddress &&
-        <Input placeholder="Enter xDai address of receiver" secondary />
-      }
-      <Button gradient onClick={() => withdraw(account, amount)}>Withdraw</Button>
+      {/* <Checkbox label="xDai address of receiver" check={isXDaiAddress} onChange={handleCheckboxClick} /> */}
+      {/* {isXDaiAddress && */}
+      <Input placeholder="Enter xDai address of receiver" secondary onChange={handleReceiverChange} />
+      {/* } */}
+      <Button gradient onClick={() => withdraw(receiver, amount)}>Withdraw</Button>
     </Card>
   );
 };
