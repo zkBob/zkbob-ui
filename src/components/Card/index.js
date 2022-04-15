d96437fb676df9162848188097e6516121536b24
Max Alekseenko <nnwen0609@gmail.com>
2022-04-15 12:35:06 +0800

    change card shadow

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -13,7 +13,7 @@ const Card = styled.div`
   display: flex;
   flex-direction: column;
   background: ${props => props.theme.card.background};
-  box-shadow: 0px 8px 24px #EBE9F3;
+  box-shadow: 0px 8px 50px rgba(255, 214, 110, 0.2);
   border-radius: 24px;
   padding: 16px 12px 12px;
   width: 480px;
