8516fb3912372272af7374a67b724c7a38f4386d
Max Alekseenko <nnwen0609@gmail.com>
2022-04-19 12:28:22 +0800

    change button text

--- src/containers/AccountSetUpButton/index.js
+++ src/containers/AccountSetUpButton/index.js
@@ -7,6 +7,6 @@ import Button from 'components/Button';
 export default () => {
   const { openAccountSetUpModal } = useContext(WalletModalContext);
   const { isLoadingZkAccount } = useContext(ZkAccountContext);
-  const text = isLoadingZkAccount ? 'Loading zero knowledge account...' : 'Set up zero knowledge account';
+  const text = isLoadingZkAccount ? 'Loading zero knowledge account...' : 'Get started!';
   return <Button loading={isLoadingZkAccount} disabled={isLoadingZkAccount} onClick={openAccountSetUpModal}>{text}</Button>;
 }
