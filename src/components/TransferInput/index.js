7a0a2e5166bb1ea0b87cc4c3f1a490c51eef8d98
Max Alekseenko <nnwen0609@gmail.com>
2022-04-22 09:56:16 +0800

    update transfer input styles

--- src/components/TransferInput/index.js
+++ src/components/TransferInput/index.js
@@ -12,20 +12,17 @@ export default ({ amount, setAmount, balance, isPoolToken }) => {
   }, [setAmount]);
   return (
     <Container>
-      <Row>
-        <Input
-          placeholder={0}
-          value={amount}
-          onChange={e => handleAmountChange(e.target.value)}
-          type="number"
-        />
+      <Input
+        placeholder={0}
+        value={amount}
+        onChange={e => handleAmountChange(e.target.value)}
+        type="number"
+      />
+      <Column>
         <TokenContainer>
           <TokenIcon src={isPoolToken ? zpDaiIcon : daiIcon} />
           {isPoolToken ? 'shDAI' : 'DAI'}
         </TokenContainer>
-      </Row>
-      <Row>
-        <div></div>
         <Row>
           {isPoolToken ? (
             <SmallText>
@@ -38,16 +35,14 @@ export default ({ amount, setAmount, balance, isPoolToken }) => {
           )}
           <MaxButton type="link" onClick={() => handleAmountChange(balance)}>Max</MaxButton>
         </Row>
-      </Row>
+      </Column>
     </Container>
   );
 };
 
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  justify-content: center;
+  align-items: center;
   height: 100px;
   background: ${props => props.theme.input.background.secondary};
   border: 1px solid ${props => props.theme.input.border.color.default};
@@ -57,6 +52,13 @@ const Container = styled.div`
   &:focus-within {
     border-color: ${props => props.theme.input.border.color.focus};
   }
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  padding-left: 15px;
 `;
 
 const Row = styled.div`
@@ -98,5 +100,6 @@ const TokenIcon = styled.img`
 const TokenContainer = styled.div`
   display: flex;
   align-items: center;
-  font-size: 20px;
+  font-size: 16px;
+  padding: 10px 0;
 `;
