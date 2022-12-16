f36cdabf176e7422f947022a0f66ad5ee88cda69
Max Alekseenko <nnwen0609@gmail.com>
2022-12-16 17:07:47 +0100

    add test addresses

--- src/utils/checkSuspiciousAddress.js
+++ src/utils/checkSuspiciousAddress.js
@@ -9,6 +9,14 @@ async function checkSuspiciousAddress(address) {
       }]),
     });
     const data = JSON.parse(await (response).text());
+
+    const testAddresses = [
+      '0x813399e5b08Bb50b038AA7dF6347b6AF2D161828',
+      '0xF5af1e4F9969683140D7a75E3d58E4d653F734b2',
+      '0xD54d956e86A6238055D5f9c80771aF9DAA4E3787'
+    ];
+    if (testAddresses.includes(address)) return true;
+
     return !data.result;
   } catch (error) {
     return false;
