2f1a55e857de58fd80b25729fa3de4aa8255cc58
Max Alekseenko <nnwen0609@gmail.com>
2023-05-19 14:47:13 +0200

    add copying to clipboard for support id

--- src/components/RedeemGiftCardModal/index.js
+++ src/components/RedeemGiftCardModal/index.js
@@ -1,17 +1,21 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import styled from 'styled-components';
 import { BigNumber } from 'ethers';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import ButtonDefault from 'components/Button';
 import Modal from 'components/Modal';
 import Link from 'components/Link';
 import Spinner from 'components/Spinner';
+import Tooltip from 'components/Tooltip';
 
 import { formatNumber } from 'utils';
 
 import { ReactComponent as BobTokenDefault } from 'assets/bob.svg';
-import { ReactComponent as CheckIconDefault } from 'assets/check-circle.svg';
+import { ReactComponent as CheckCircleIconDefault } from 'assets/check-circle.svg';
 import { ReactComponent as CrossIconDefault } from 'assets/cross-circle.svg';
+import { ReactComponent as CopyIconDefault } from 'assets/copy.svg';
+import { ReactComponent as CheckIconDefault } from 'assets/check.svg';
 
 import config from 'config';
 import { NETWORKS } from 'constants';
@@ -25,6 +29,30 @@ const COMPLETED = 5;
 const FAILED = 6;
 
 const gradient = 'linear-gradient(150deg, rgba(251, 237, 206, 0.8) 10%, rgba(250, 243, 230, 0.5) 50%, rgba(251, 237, 206, 0.8) 100%)';
+
+const SupportId = ({ supportId }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const onCopy = useCallback((text, result) => {
+    if (result) {
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 2000);
+    }
+  }, []);
+
+  return (
+    <CopyToClipboard text={supportId} onCopy={onCopy}>
+      <SupportIdContainer>
+        <Description style={{ marginRight: 8 }}>
+          Support ID: {supportId}
+        </Description>
+        <Tooltip content="Copied" placement="right" visible={isCopied}>
+          {isCopied ? <CheckIcon /> : <CopyIcon />}
+        </Tooltip>
+      </SupportIdContainer>
+    </CopyToClipboard>
+  );
+};
 
 const LoadingScreen = () => (
   <>
@@ -104,7 +132,7 @@ const InProgressScreen = ({ supportId }) => {
             The process takes longer than usual. If the process does not complete within a few seconds,{' '}
             contact our <Link size={16} href="https://zkbob.canny.io/report-issue/">support team</Link>
           </Description>
-          <Description>Support ID: {supportId}</Description>
+          <SupportId supportId={supportId} />
         </>
       )}
     </>
@@ -113,7 +141,7 @@ const InProgressScreen = ({ supportId }) => {
 
 const CompletedScreen = ({ close }) => (
   <>
-    <CheckIcon />
+    <CheckCircleIcon />
     <StatusTitle style={{ marginBottom: 16, marginTop: 0 }}>
      Your BOB is on the way
     </StatusTitle>
@@ -134,7 +162,7 @@ const FailedScreen = ({ supportId }) => (
     <Description style={{ marginBottom: 16 }}>
       Please contact our support team to resolve this problem. Use Support ID in your request.
     </Description>
-    <Description>Support ID: {supportId}</Description>
+    <SupportId supportId={supportId} />
     <Button onClick={() => window.open('https://zkbob.canny.io/report-issue/', '_blank')}>
       Contact support
     </Button>
@@ -270,7 +298,7 @@ const StatusTitle = styled.span`
   text-align: center;
 `;
 
-const CheckIcon = styled(CheckIconDefault)`
+const CheckCircleIcon = styled(CheckCircleIconDefault)`
   margin-bottom: 20px;
   margin-top: -24px;
 `;
@@ -299,4 +327,24 @@ const ProgressText = styled.span`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+`;
+
+const CopyIcon = styled(CopyIconDefault)`
+  width: 16px;
+  height: 16px;
+`;
+
+const CheckIcon = styled(CheckIconDefault)`
+  width: 16px;
+  height: 16px;
+`;
+
+const SupportIdContainer = styled.div`
+  text-align: center;
+  cursor: pointer;
+  &:hover ${CopyIcon} {
+    path {
+      fill: ${props => props.theme.color.purple};
+    }
+  }
 `;
