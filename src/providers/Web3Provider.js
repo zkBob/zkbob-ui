2a5c069c595792a3214b670e3cea21d82a645ba8
Max Alekseenko <nnwen0609@gmail.com>
2023-06-26 19:23:03 +0200

    enable WC v2 and disable v1

--- src/providers/Web3Provider.js
+++ src/providers/Web3Provider.js
@@ -2,11 +2,10 @@ import { WagmiConfig, configureChains, createClient } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
 import { sepolia, polygon, goerli, optimism, optimismGoerli } from 'wagmi/chains';
 import { InjectedConnector } from 'wagmi/connectors/injected';
-// import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
-import { WalletConnectLegacyConnector } from 'wagmi/connectors/walletConnectLegacy';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [sepolia, polygon, goerli, optimism, optimismGoerli],
+  process.env.REACT_APP_CONFIG === 'dev' ? [sepolia, goerli, optimismGoerli] : [polygon, optimism],
   [publicProvider()],
 );
 
@@ -16,27 +15,22 @@ const injected = new InjectedConnector({
     name: 'MetaMask',
   },
 });
-const walletConnectV1 = new WalletConnectLegacyConnector({
+
+const walletConnect = new WalletConnectConnector({
   chains,
   options: {
     qrcode: true,
+    projectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
+    name: 'zkBob',
+    relayUrl: 'wss://relay.walletconnect.org'
   },
 });
-// const walletConnectV2 = new WalletConnectConnector({
-//   chains,
-//   options: {
-//     qrcode: true,
-//     projectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
-//     name: 'zkBob',
-//     relayUrl: 'wss://relay.walletconnect.org'
-//   },
-// });
 
 const client = createClient({
   autoConnect: true,
   provider,
   webSocketProvider,
-  connectors: [injected, walletConnectV1, /*walletConnectV2*/],
+  connectors: [injected, walletConnect],
 });
 
 export default ({ children }) => (
