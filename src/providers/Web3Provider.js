b550594cbf53e27d0696e35f64dce0c96f55e20c
Max Alekseenko <nnwen0609@gmail.com>
2023-07-12 14:38:41 +0200

    add ankr rpc for optimism

--- src/providers/Web3Provider.js
+++ src/providers/Web3Provider.js
@@ -1,12 +1,21 @@
 import { WagmiConfig, configureChains, createClient } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import { sepolia, polygon, goerli, optimism, optimismGoerli } from 'wagmi/chains';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 
 const { chains, provider, webSocketProvider } = configureChains(
   process.env.REACT_APP_CONFIG === 'dev' ? [sepolia, goerli, optimismGoerli] : [polygon, optimism],
-  [publicProvider()],
+  [
+    jsonRpcProvider({
+      priority: 0,
+      rpc: chain => chain.id === optimism.id
+        ? ({ http: 'https://rpc.ankr.com/optimism' })
+        : null,
+    }),
+    publicProvider({ priority: 1 }),
+  ],
 );
 
 const injected = new InjectedConnector({
