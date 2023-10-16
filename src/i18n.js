8a7b6c6ef2468471d86c82ef22b1e91a054fe756
Max Alekseenko <nnwen0609@gmail.com>
2023-10-16 17:01:09 +0200

    load translations in another way

--- src/i18n.js
+++ src/i18n.js
@@ -1,15 +1,21 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 i18n
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: ['en', 'pt'],
     nonExplicitSupportedLngs: true,
+    resources: {
+      en: {
+        translation: require('translations/en.json'),
+      },
+      pt: {
+        translation: require('translations/pt.json'),
+      },
+    },
   });
 
 export default i18n;
