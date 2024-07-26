93799951b6c5614f2b8fd57a70cb5f801c313978
vladimir <vladimir@blockscout.com>
2024-07-26 20:24:40 +0300

    gets rpc from config

--- src/providers/Web3Provider.js
+++ src/providers/Web3Provider.js
@@ -1,41 +1,43 @@
 import { WagmiConfig, configureChains, createClient } from 'wagmi';
-import { publicProvider } from 'wagmi/providers/public';
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import { sepolia, polygon, goerli, optimism, optimismGoerli } from 'wagmi/chains';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+import config from  '../config'
 
+
+const getRpcByPriority = (priority) => {
+
+  return ({id}) => {
+    const allChains = Array.from(Object.entries(config.chains));
+    const [_, selectedChain] = allChains.find(([chainId, _ ]) => chainId == id);
+    const len = selectedChain?.rpcUrls.length;
+    let res;
+    if (len > priority-1)
+     {
+       res =  ({http: selectedChain.rpcUrls[priority]})
+    } else {
+       res = ({http: selectedChain.rpcUrls[priority%len]})
+    };
+    return res;
+  }
+
+}
+
+const networks = process.env.REACT_APP_CONFIG === 'dev' ? [sepolia, goerli, optimismGoerli] : [polygon, optimism];
+console.log('networks', networks)
 const { chains, provider, webSocketProvider } = configureChains(
-  process.env.REACT_APP_CONFIG === 'dev' ? [sepolia, goerli, optimismGoerli] : [polygon, optimism],
+  networks,
   [
     jsonRpcProvider({
       priority: 0,
-      rpc: chain => {
-        if (chain.id === optimism.id) {
-          return ({ http: 'https://rpc.ankr.com/optimism' });
-        }
-        if (chain.id === sepolia.id) {
-          return ({ http: 'https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161' });
-        }
-        if (chain.id === polygon.id) {
-          return ({ http: 'https://rpc.ankr.com/polygon' });
-        }
-        return null;
-      }
-    }),
-    publicProvider({ priority: 1 }),
+      rpc: getRpcByPriority(0)}),
+      jsonRpcProvider({priority:1,
+        rpc: getRpcByPriority(1)}),
     jsonRpcProvider({priority:2,
-      rpc: chain => {
-        if (chain.id == optimism.id) {
-          return ({http: 'https://optimism-mainnet.infura.io/v3/9a94d181b23846209f01161dcd0f9ad6'})
-        }
-      } }),
-    jsonRpcProvider({priority:3,
-      rpc: chain => {
-        if (chain.id == optimism.id) {
-          return ({http: 'https://opt-mainnet.g.alchemy.com/v2/fnUqqB1tThPuSpM33VFm26wqXISKPS2n'})
-        }
-      } }),
+      rpc: getRpcByPriority(2)}),
+      jsonRpcProvider({priority:3,
+        rpc: getRpcByPriority(3)})
   ],
 );
 
