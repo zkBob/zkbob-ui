16cfa8e834deb819c481e44ca81596d458dd61d9
vladimir <vladimir@blockscout.com>
2025-06-28 14:35:56 +0300

    update OP RPC

--- src/config/index.js
+++ src/config/index.js
@@ -80,7 +80,7 @@ const config = {
         rpcUrls: ['https://rpc.ankr.com/polygon', 'https://polygon-rpc.com', 'https://polygon-mainnet.g.alchemy.com/v2/fnUqqB1tThPuSpM33VFm26wqXISKPS2n'],
       },
       '10': {
-        rpcUrls: ['https://mainnet.optimism.io','https://optimism.llamarpc.com','https://optimism.gateway.tenderly.co'],
+        rpcUrls: ['https://mainnet.optimism.io','https://optimism-mainnet.public.blastapi.io','https://optimism.gateway.tenderly.co','https://op-pokt.nodies.app','https://optimism-rpc.publicnode.com'],
       },
     },
     "snarkParamsSet": {
